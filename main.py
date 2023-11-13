eaeb3d432ee416947d1e86072a561b2cf4565093
AndrewPolyakov1 <mr.dijoy@gmail.com>
2023-11-13 23:47:12 +0300

    Tokenize function in the lib

--- main.py
+++ main.py
@@ -2,11 +2,14 @@ import ASTlib
 import ASTlib.parse as parser
 import os
 import json
+import ast
 
 file_test = '/examples/ex1.py'
+
 
 if __name__ == '__main__':
     path = os.path.dirname(os.path.abspath(__file__))
     path += file_test
     jt = ASTlib.getAST(path)
-    print(json.dumps(parser.data_from_module(jt), indent=4))
+    s = ASTlib.tokenize(path)
+    print(json.dumps(s, indent=4))
