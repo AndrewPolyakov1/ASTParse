874d236148889acbb7caee56c10c49bdc3f0e2ed
unknown <axtra2@gmail.com>
2023-11-15 16:18:43 +0300

    better import

--- ASTlib/__init__.py
+++ ASTlib/__init__.py
@@ -3,6 +3,12 @@ Module for parsing code into pseudocode and ploting CFG.
 """
 
 from .tokenize import tokenize
-from .interface import translate, build_cfg_config, create_image_from_config, code_to_image_and_pseudocode, change_keys_colors
+from .interface import (
+    translate,
+    build_cfg_config,
+    create_image_from_config,
+    code_to_image_and_pseudocode,
+    change_keys_colors
+)
 from .pseudo import parse, unparse
 from .py2cfg_ext import CFGBuilder
